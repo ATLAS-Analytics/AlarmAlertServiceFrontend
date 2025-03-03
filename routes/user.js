33bcb3a903bc84414bd5ffcba77b17a865d9bd53
Ilija Vukotic <ivukotic@uchicago.edu>
2025-03-03 11:21:49 -0600

    Update dependencies and GitHub Actions checkout version

--- routes/user.js
+++ routes/user.js
@@ -167,9 +167,6 @@ router.post('/preferences/:userId', jsonParser, async (req, res) => {
       console.log(`${key} not allowed.\n`);
       disallowed += `${key} not allowed.\n`;
     }
-    if (typeof value !== config.PREFERENCES[key].type) {
-      console.log(`Warning! ${key} has wrong type. It should be ${config.PREFERENCES[key].type}\n`);
-    }
   });
   if (disallowed.length > 0) {
     res.status(400).send(disallowed);
